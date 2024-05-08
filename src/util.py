036601dc6d19c29eed8652e9aac2f04f9c50e564
kuiyuanc <kono.c@nycu.edu.tw>
2024-05-08 11:29:03 +0800

    feat: EpsilonGreedy

---
+++ src/util.py
@@ -0,0 +1,14 @@
+import random
+
+
+class EpsilonGreedy:
+    def __init__(self, epsilon: float = 1, decay: float = 0.995, min_epsilon: float = 0.05) -> None:
+        self._epsilon = epsilon
+        self._decay = decay
+        self._min = min_epsilon
+
+    def __call__(self, *, explore, exploit):
+        return explore if random.random() < self._epsilon else exploit
+
+    def decay(self) -> None:
+        self._epsilon = max(self._min, self._epsilon * self._decay)
